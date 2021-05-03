cec04656a23283c48314ea5940a2da03e103b029
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2021-05-03 21:52:07 +0200

    putting const into functions

--- js/script.js
+++ js/script.js
@@ -3,22 +3,23 @@
     console.log("Hello programmer!");
   };
 
-  const buttonOk = document.querySelector(".js-header__button");
-  const buttonInfo = document.querySelector(".js-section__buttonInfo");
-
   const hideNotification = () => {
     const notification = document.querySelector(".js-header__notification");
+    const buttonOk = document.querySelector(".js-header__button");
     notification.remove();
     buttonOk.remove();
   };
 
   const hideFoldedSection = () => {
     const infoUnfold = document.querySelector(".js-section__foldUnfold");
+    const buttonInfo = document.querySelector(".js-section__buttonInfo");
     infoUnfold.classList.toggle("section__hidden");
     buttonInfo.innerText = buttonInfo.innerText === "Pokaż" ? "Ukryj" : "Pokaż";
   };
 
   const init = () => {
+    const buttonOk = document.querySelector(".js-header__button");
+    const buttonInfo = document.querySelector(".js-section__buttonInfo");
     buttonOk.addEventListener("click", hideNotification);
     buttonInfo.addEventListener("click", hideFoldedSection);
   };
