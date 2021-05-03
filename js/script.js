38b54567f23519a9b27aed1ad7119372886d82c4
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2021-05-03 21:12:01 +0200

    added brackets

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
-const welcome = () => {
-  console.log("Hello programmer!");
-};
-
-let buttonOk = document.querySelector(".header__button");
-let notification = document.querySelector(".header__notification");
-
-buttonOk.addEventListener("click", () => {
-  notification.remove();
-  buttonOk.remove();
-});
-
-let buttonInfo = document.querySelector(".section__buttonInfo");
-let infoUnfold = document.querySelector(".section__unfold");
-
-buttonInfo.addEventListener("click", () => {
-  infoUnfold.classList.toggle("section__hidden");
-  if (buttonInfo.innerText === "Pokaż") {
-    buttonInfo.innerText = "Ukryj";
-  } else {
-    buttonInfo.innerText = "Pokaż";
-  }
-});
-
-welcome();
+{
+  const welcome = () => {
+    console.log("Hello programmer!");
+  };
+  
+  let buttonOk = document.querySelector(".header__button");
+  let notification = document.querySelector(".header__notification");
+  
+  buttonOk.addEventListener("click", () => {
+    notification.remove();
+    buttonOk.remove();
+  });
+  
+  let buttonInfo = document.querySelector(".section__buttonInfo");
+  let infoUnfold = document.querySelector(".section__unfold");
+  
+  buttonInfo.addEventListener("click", () => {
+    infoUnfold.classList.toggle("section__hidden");
+    if (buttonInfo.innerText === "Pokaż") {
+      buttonInfo.innerText = "Ukryj";
+    } else {
+      buttonInfo.innerText = "Pokaż";
+    }
+  });
+  
+  welcome();
+  
+}
