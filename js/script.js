a632c8fd94730b5db77e9b51ad6ed357786ac54c
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2021-05-17 22:33:15 +0200

    improved JS names and simplified html

--- js/script.js
+++ js/script.js
@@ -5,21 +5,21 @@
 
   const hideNotification = () => {
     const notification = document.querySelector(".js-header__notification");
-    const buttonOk = document.querySelector(".js-header__button");
+    const buttonOk = document.querySelector(".js-buttonOk");
     notification.remove();
     buttonOk.remove();
   };
 
   const hideFoldedSection = () => {
     const infoUnfold = document.querySelector(".js-section__foldUnfold");
-    const buttonInfo = document.querySelector(".js-section__buttonInfo");
+    const buttonInfo = document.querySelector(".js-buttonInfo");
     infoUnfold.classList.toggle("section__hidden");
     buttonInfo.innerText = buttonInfo.innerText === "Pokaż" ? "Ukryj" : "Pokaż";
   };
 
   const init = () => {
-    const buttonOk = document.querySelector(".js-header__button");
-    const buttonInfo = document.querySelector(".js-section__buttonInfo");
+    const buttonOk = document.querySelector(".js-buttonOk");
+    const buttonInfo = document.querySelector(".js-buttonInfo");
     buttonOk.addEventListener("click", hideNotification);
     buttonInfo.addEventListener("click", hideFoldedSection);
   };
