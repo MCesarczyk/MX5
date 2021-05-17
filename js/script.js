c9df1322aaf643c5a2beebd71c0d5ed1849e360f
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2021-05-17 22:38:10 +0200

    button declarations moved up

--- js/script.js
+++ js/script.js
@@ -3,23 +3,23 @@
     console.log("Hello programmer!");
   };
 
+  const buttonOk = document.querySelector(".js-buttonOk");
+  const buttonInfo = document.querySelector(".js-buttonInfo");
+
+
   const hideNotification = () => {
     const notification = document.querySelector(".js-header__notification");
-    const buttonOk = document.querySelector(".js-buttonOk");
     notification.remove();
     buttonOk.remove();
   };
 
   const hideFoldedSection = () => {
     const infoUnfold = document.querySelector(".js-section__foldUnfold");
-    const buttonInfo = document.querySelector(".js-buttonInfo");
     infoUnfold.classList.toggle("section__hidden");
     buttonInfo.innerText = buttonInfo.innerText === "Pokaż" ? "Ukryj" : "Pokaż";
   };
 
   const init = () => {
-    const buttonOk = document.querySelector(".js-buttonOk");
-    const buttonInfo = document.querySelector(".js-buttonInfo");
     buttonOk.addEventListener("click", hideNotification);
     buttonInfo.addEventListener("click", hideFoldedSection);
   };
